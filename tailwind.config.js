9e84b33b752b73400b9d548272f24ced644bc87b
Azim <azimuddin807@gmail.com>
2024-08-21 16:10:43 +0600

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+        colors: {
+            'deers': '#B68C5A',
+          },
     },
-    plugins: [],
-  }
+    
+  },
+  plugins: [],
+}
